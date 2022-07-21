9a2fa2b51ec19aa7d8cace6e5af80b93afe81579
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-07-21 14:10:20 +0700

    Handle CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('colors');
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(morgan('dev'));
 
 // Product routes
